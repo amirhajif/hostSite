fbce0e28422d4dc00e39b38b6f01ac4d3f595cbf
amirhajif <58939938+amirhajif@users.noreply.github.com>
2021-08-20 22:11:16 +0430

    upload new databse file

--- _host_ .sql
+++ _host_ .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 19, 2021 at 06:59 PM
+-- Generation Time: Aug 20, 2021 at 05:40 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -22,6 +22,27 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `hostsite` DEFAULT CHARACTER SET utf8 COLLATE utf8_persian_ci;
 USE `hostsite`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `email` varchar(40) COLLATE utf8_persian_ci NOT NULL,
+  `username` varchar(40) COLLATE utf8_persian_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8_persian_ci NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`email`, `username`, `password`) VALUES
+('amirhh@gmail.com', 'amir.h.h', '12345678');
 
 -- --------------------------------------------------------
 
@@ -45,6 +66,26 @@ INSERT INTO `users` (`email`, `phoneNumber`, `password`) VALUES
 ('amir@gmail.com', 0, '0'),
 ('amirhajitabar2@gmail.com', 1, '1'),
 ('aa@gmail.com', 0, '12345678');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitors`
+--
+
+DROP TABLE IF EXISTS `visitors`;
+CREATE TABLE IF NOT EXISTS `visitors` (
+  `date` date NOT NULL,
+  `ip` varchar(40) COLLATE utf8_persian_ci NOT NULL,
+  PRIMARY KEY (`ip`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `visitors`
+--
+
+INSERT INTO `visitors` (`date`, `ip`) VALUES
+('2021-08-20', '::1');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
