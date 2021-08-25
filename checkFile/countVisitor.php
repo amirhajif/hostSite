9da79e46ba10b4da8b223a34ba903603fd1126a9
amirhajif <58939938+amirhajif@users.noreply.github.com>
2021-08-25 23:08:14 +0430

    fix visitors bug

--- checkFile/countVisitor.php
+++ checkFile/countVisitor.php
@@ -6,9 +6,9 @@ try {
 }
 $date=date("Y-m-d");
 $ip=$_SERVER['REMOTE_ADDR'];
-$query="SELECT * FROM visitors WHERE date='$date' OR ip='$ip'";
+$query="SELECT * FROM visitors WHERE date='$date'";
 $result=mysqli_query($link,$query);
-if (mysqli_num_rows($result) == 0)
+if ($result->num_rows == 0)
 {
     $query="INSERT INTO visitors (date,ip) VALUES ('$date','$ip')";
     mysqli_query($link,$query);
